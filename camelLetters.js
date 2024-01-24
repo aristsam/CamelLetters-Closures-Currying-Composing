0c8674abf645216f0e3578e88ea38e2b23dfa96d
Aris <72753404+aristsam@users.noreply.github.com>
2024-01-24 19:26:11 +0200

    Update camelLetters.js

--- camelLetters.js
+++ camelLetters.js
@@ -13,3 +13,18 @@ const CamelLetters= (string) => {
 console.log(CamelLetters("HEEELLooo"));
 console.log(CamelLetters("Aris"));
 console.log(CamelLetters("angelo"));
+
+
+//CLOSURES - a function ran.The function executed. it's never going to execute again.
+//But it's going to remember that there are references to those variables.
+//So the child scope always has access to the parent scope.Example: 
+function first() {
+  const name = "Aris";
+  function displayName() {
+    console.log(name);
+  }
+  return displayName;
+}
+
+const myFunc = first();
+myFunc();
